29b3e0bd2071264c44ffc7d8baf2541b3c6c031f
Arvida Jimenez <ajimenez@ita-5268.schoologize.com>
2018-04-18 15:53:04 -0400

    initial commit

---
+++ challenge.intdoubled.py
@@ -0,0 +1,7 @@
+# Given and array of integers (x), return the array with each value doubled.
+
+def maps(a):
+    x = []
+    for num in a:
+        x.append(num * 2)
+    return x
---
+++ challenge.oddint.py
@@ -0,0 +1,7 @@
+# Given an array, find the int that appears an odd number of times.
+# There will always be only one integer that appears an odd number of times.
+
+def find_it(seq):
+    for num in seq:
+        if seq.count(num) >= 1 and (seq.count(num) % 2 != 0):
+            return num
